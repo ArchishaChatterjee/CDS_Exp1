fcc92e20ec11453f2542758a97bda62f1a97d788
Archisha Chatterjee <archisha.chatterjee.btech2024@sitpune.edu.in>
2025-07-15 23:24:12 +0530

    Create Exp1B.cpp

---
+++ Exp1B.cpp
@@ -0,0 +1,19 @@
+// Experiment 1B : Calculator 
+// Archisha Chatterjee
+// PRN : 24070123021
+
+# include <iostream>
+using namespace std;
+
+int main(){
+float a,b;
+cout << "Enter first number :";
+cin >> a;
+cout << "Enter second number :';
+cin >> b;
+cout << "Sum is " << a+b;
+cout << "Subtraction is " << a-b;
+cout << "Product is " << a*b;
+cout << "Division is " << a/b;
+return 0;
+}
